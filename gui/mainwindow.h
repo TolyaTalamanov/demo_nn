eaa4df0323a37eaad78295619df9bbc1fc3e0767
tolik <talamtolik@gmail.com>
2018-03-05 01:17:50 +0300

    Add Main window start api

---
+++ gui/mainwindow.h
@@ -0,0 +1,14 @@
+#ifndef DEMO_NN_GUI_MAINWINDOW_H
+#define DEMO_NN_GUI_MAINWINDOW_H
+
+#include <QtWidgets>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow();
+    ~MainWindow();
+};
+#endif //DEMO_NN_GUI_MAINWINDOW_H
