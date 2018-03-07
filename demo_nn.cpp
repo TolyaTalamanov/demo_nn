f35ea5a265dd23af95a6c4b75c69e3ef37d9286f
tolik <talamtolik@gmail.com>
2018-03-08 01:17:37 +0300

    Add sample

---
+++ demo_nn.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+#include "gui/mainwindow.h"
+
+int main(int argc, char* argv[]) {
+  QApplication app(argc, argv);
+  MainWindow* mainWindow = new MainWindow;
+  mainWindow->show();
+  return app.exec();
+}
